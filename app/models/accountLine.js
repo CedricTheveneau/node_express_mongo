00303fbeb85b967d141461e7c9a588c0f7cdfdb8
Cédric Theveneau <87996087+CedricTheveneau@users.noreply.github.com>
2024-04-17 14:34:47 +0200

    Added validation options for each model

--- app/models/accountLine.js
+++ app/models/accountLine.js
@@ -1,15 +1,46 @@
 const mongoose = require("mongoose");
 
 const accountLineSchema = new mongoose.Schema({
-  label: { type: String },
-  action: { type: String },
-  amount: { type: Number },
-  date: { type: Date },
-  method: { type: String },
-  status: { type: Boolean },
-  category: { type: String },
-  lastUpdated: { type: Date },
-  account: { type: String },
+  label: {
+    type: String,
+    required: [true, "The label field is required"],
+    maxlength: [50, "The label must be at most 50 characters long"],
+    minlength: [2, "The category name must be at least 2 characters long"],
+  },
+  action: {
+    type: String,
+    required: [true, "The action field is required"],
+    enum: {
+      value: ["Crédit", "Débit"],
+      message: "{VALUE} is not an available option",
+    },
+  },
+  amount: {
+    type: Number,
+    required: [true, "The amount field is required"],
+    min: [1, "The amount must be at least 1€"],
+  },
+  date: { type: Date, required: [true, "The date field is required"] },
+  method: {
+    type: String,
+    required: [true, "The method field is required"],
+    enum: {
+      value: ["Chèque", "Retrait", "CB", "Virement"],
+      message: "{VALUE} is not an available option",
+    },
+  },
+  status: { type: Boolean, default: false },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+    required: [true, "The category field is required"],
+  },
+  lastUpdated: { type: Date, required: [true, "The date field is required"] },
+  account: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Account",
+    required: [true, "The account field is required"],
+  },
 });
 
 const AccountLine = mongoose.model("AccountLine", accountLineSchema);
--- app/models/category.js
+++ app/models/category.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 const categorySchema = new mongoose.Schema({
-  name: { type: String },
+  name: {
+    type: String,
+    required: [true, "The name field is required"],
+    unique: [true, "The name field must be unique"],
+    minlength: [5, "The category name must be at least 5 characters long"],
+  },
 });
+
+categorySchema.plugin(uniqueValidator);
 
 const Category = mongoose.model("Category", categorySchema);
 
